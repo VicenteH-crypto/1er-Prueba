4ffc7db326d41365b2deb7dd9a0e19e61c82619c
VicenteH-crypto <vi.huenul@duocuc.cl>
2025-04-09 09:34:31 -0400

    clase02 python
    suma, resta, multiplicación y división

---
+++ clase02 python.py
@@ -0,0 +1,27 @@
+# Definir
+
+V01 = int(input('Dime un numero entero: '))
+V02 = float(input('Dime un numero decimal: '))
+
+# Operación
+
+R01 = V01 + V02
+
+# Salida
+
+V01 = int(input('Dime un numero entero para restar: '))
+V02 = float(input('Ahora un numero decimal: '))
+
+R02 = V01 - V02
+
+V01 = int(input('Dime un numero entero para multiplicar: '))
+V02 = float(input('Ahora un numero decimal: '))
+
+R03 = V01 * V02
+
+V01 = int(input('Dime un numero entero para dividir: '))
+V02 = float(input('Ahora un numero decimal: '))
+
+R04 = V01 / V02
+
+print(f'El resultado es: {R01, R02, R03, R04}')
